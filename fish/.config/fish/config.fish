5208e0fd72160a5ea7b41f20cd8a920b13282fa6
Teru <26590954+Terunori-Hashizume@users.noreply.github.com>
2026-02-21 09:02:55 +0900

    fishの設定追加

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,22 +8,33 @@ set fish_greeting # No greeting messages
 
 # colortheme
 fish_config theme choose "Rosé Pine Dawn"
+if type -q vivid
+    set -gx LS_COLORS (vivid generate rose-pine-dawn)
+end
 
 # prompt
-starship init fish | source
+if type -q starship
+    starship init fish | source
+end
 
 # directory navigation
-zoxide init fish | source
+if type -q zoxide
+    zoxide init fish | source
+end
 
 # file manager
-function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    command yazi $argv --cwd-file="$tmp"
-    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
-        builtin cd -- "$cwd"
+if type -q yazi
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
     end
-    rm -f -- "$tmp"
 end
 
 # fzf.fish (plugin)
-set fzf_preview_dir_cmd eza --all --color=always
+if type -q eza
+    set fzf_preview_dir_cmd eza --all --color=always
+end
